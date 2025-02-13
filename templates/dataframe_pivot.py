a714c84a2bf3696825977e6619899c1c659bc7f4
JordiCremades4444 <jordicremadesrosell@gmail.com>
2025-02-13 20:15:45 +0100

    Add DataFramePivot template for pivoting and aggregating data

---
+++ templates/dataframe_pivot.py
@@ -0,0 +1,17 @@
+# Create an instance of DataFramePivot
+dp = dataframe_pivot.DataFramePivot(df)
+
+TIME = 'XXX'
+DIMENSIONS = ['XXX', 'XXX']
+MEASURES = ['XXX', 'XXX']
+AFFUNCS = ['sum','mean']
+
+# Call the pivot_and_aggregate method
+p = dp.pivot_and_aggregate(
+    t=TIME,
+    dimensions=DIMENSIONS,
+    measures=MEASURES,
+    aggfuncs=AFFUNCS
+)
+
+p
