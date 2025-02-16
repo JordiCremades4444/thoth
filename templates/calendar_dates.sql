340e334dee7c47777554c74d422bf53c7feaec69
JordiCremades4444 <jordicremadesrosell@gmail.com>
2025-02-16 18:39:54 +0100

    Fix calendar_dates SQL template to use end_date for date range

--- templates/calendar_dates.sql
+++ templates/calendar_dates.sql
@@ -1,5 +1,5 @@
 with calendar_dates as (select
     calendar_date
-    from unnest(sequence(date({start_date}),date({calendar_date}),interval '1' day)) as cte (calendar_date)
+    from unnest(sequence(date({start_date}),date({end_date}),interval '1' day)) as cte (calendar_date)
     where true
 )
