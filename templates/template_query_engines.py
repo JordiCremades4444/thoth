aa63152a56cbf9d91cdd2e0b52fece1aef46992a
JordiCremades4444 <jordicremadesrosell@gmail.com>
2025-03-28 14:22:52 +0100

    Add BigQuery queries

--- templates/template_query_engines.py
+++ templates/template_query_engines.py
@@ -8,7 +8,7 @@ END_DATE = "'2026-01-01'"
 
 params = {"start_date": str(START_DATE), "end_date": str(END_DATE)}
 
-df = q.run_query_starburst(
+df = q.run_query_starburst(  # or run_table_explorer_bigquery
     QUERY_NAME, params=params, csv_file=QUERY_NAME, load_csv_file=False
 )
 
@@ -20,4 +20,4 @@ df.head()
 
 TABLE = "delta.central_order_descriptors_odp.order_descriptors_v2"
 
-df = q.run_table_explorer_starburst(TABLE)
+df = q.run_table_explorer_starburst(TABLE)  # or run_table_explorer_bigquery
